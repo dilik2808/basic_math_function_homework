33b4bad1d7a70bac6fcec49c6e8d0428d9138e3c
dilik2808 <dilik2808@gmail.com>
2022-10-10 12:49:21 +0500

    18

--- basic_func18.py
+++ basic_func18.py
@@ -2,9 +2,10 @@
 # Create function arguments a.
 # Round the result to 2 decimal places.
 # Assign the value pi to the variable "a" and return.
-from math import pi
+import math
 def main (a):
-    return round (a, 2)
-print(main(pi))
+
+    return round((math.pi), 2)
+print(main(3.14))
 
 
